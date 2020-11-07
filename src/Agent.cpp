ba18ff057032bdd1462f4172fdf8ee0a7804e114
naveben <naveb@gmail.com>
2020-11-07 18:09:37 +0200

    some few changes

--- src/Agent.cpp
+++ src/Agent.cpp
@@ -11,29 +11,29 @@ bool Agent:: getMakeChanges() const{
 }
 ContactTracer:: ContactTracer(): Agent(){}
 Virus:: Virus(int nodeInd): Agent(), nodeInd(nodeInd) {}
-void Virus::act(Session &session) {
-    MakeChanges= false;
-    int v= this->nodeInd;
-    Graph* current= session.getPointerGraph();
-    if (!(current->isInfected(v))) {
-        MakeChanges=true;
-        current->infectNode(v);
-        session.enqueueInfected(v);
-    }
-    vector<int> currentNodeEdges= current->getEdgesOf(v);
-    int size= currentNodeEdges.size();
-    for (int i=0;i<=size;i=+1){
-        if(i!=v&& currentNodeEdges.at(i)==1&&current->NodeStatus(v)==0){
-            MakeChanges=true;
-          current->CarryNode(i);
-          Virus* virus= new Virus(i);
-          session.addAgent(*virus);
-            delete virus;
-            break;
-        }
-    }
-}
-void ContactTracer::act(Session &session) {}
+//void Virus::act(Session &session) {
+//    MakeChanges= false;
+//    int v= this->nodeInd;
+//    Graph* current= session.getPointerGraph();
+//    if (!(current->isInfected(v))) {
+//        MakeChanges=true;
+//        current->infectNode(v);
+//        session.enqueueInfected(v);
+//    }
+//    vector<int> currentNodeEdges= current->getEdgesOf(v);
+//    int size= currentNodeEdges.size();
+//    for (int i=0;i<=size;i=+1){
+//        if(i!=v&& currentNodeEdges.at(i)==1&&current->NodeStatus(v)==0){
+//            MakeChanges=true;
+//          current->CarryNode(i);
+//          Virus* virus= new Virus(i);
+//          session.addAgent(*virus);
+//            delete virus;
+//            break;
+//        }
+//    }
+//}
+//void ContactTracer::act(Session &session) {}
 Agent *ContactTracer:: clone() const {
     return new ContactTracer();
 }
