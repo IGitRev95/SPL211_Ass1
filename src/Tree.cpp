95e61e4d1f1c0b341c412c4e9fc81073d72fa413
IGitRev95 <idorev@post.bgu.ac.il>
2020-11-07 18:20:09 +0200

    Tree on comments for run check perpesses

--- src/Tree.cpp
+++ src/Tree.cpp
@@ -13,26 +13,26 @@ Tree::Tree(int node, const vector<Tree *> &children) : node(node), children(chil
 //    std::vector<std::vector<int>> verticesInfo;
 //}
 
- Tree* Tree::createTree(const Session &session, int rootLabel){
-     switch (session.getTreeType()) {
-         case Root:
-         {
-             return new RootTree(rootLabel);//Should be deleted by CT
-         }
-         case MaxRank:
-         {
-             return new MaxRankTree(rootLabel);//Should be deleted by CT
-         }
-         case Cycle:
-         {
-             return new CycleTree(rootLabel,session.get_cycleCurrNum());//Should be deleted by CT
-         }
-     }
-}
-
-//RootTree methods implementation
-RootTree::RootTree(int rootLabel):Tree(rootLabel){}
-
-int RootTree::traceTree() {return node;}
-//---------------------------------
+// Tree* Tree::createTree(const Session &session, int rootLabel){
+//     switch (session.getTreeType()) {
+//         case Root:
+//         {
+//             return new RootTree(rootLabel);//Should be deleted by CT
+//         }
+//         case MaxRank:
+//         {
+//             return new MaxRankTree(rootLabel);//Should be deleted by CT
+//         }
+//         case Cycle:
+//         {
+//             return new CycleTree(rootLabel,session.get_cycleCurrNum());//Should be deleted by CT
+//         }
+//     }
+//}
+//
+////RootTree methods implementation
+//RootTree::RootTree(int rootLabel):Tree(rootLabel){}
+//
+//int RootTree::traceTree() {return node;}
+////---------------------------------
 
