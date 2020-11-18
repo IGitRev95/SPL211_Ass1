a858c162d6d293ab81c9aa30e37485199d3be0ff
IGitRev95 <idorev@post.bgu.ac.il>
2020-11-19 01:41:16 +0200

    all comments

--- src/main.cpp
+++ src/main.cpp
@@ -2,26 +2,13 @@
 #include "include/Session.h"
 
 using namespace std;
-Session foo(Session session);
+
 int main(int argc, char** argv){
-//    if(argc != 3){
-//        cout << "usage cTrace <config_path>" << endl;
-//        return 0;
-//    }
-
-    Session sess5= Session(argv[1]);
-    Session sess4= Session(argv[1]);
-    sess4.getGraphReference().disconnectNode(0);
-    sess4=sess5;
-    sess4.simulate();
-
+    if(argc != 2){
+        cout << "usage cTrace <config_path>" << endl;
+        return 0;
+    }
+    Session sess(argv[1]);
+    sess.simulate();
     return 0;
-
 }
-
-Session foo(Session session){
-    Session sesstry(session);
-    sesstry.clean();
-    return sesstry;
-}
-
